29a4bcb3d6760f5a8e74a9f4403a4928a8859111
lamouche <clement.cisterne@outlook.fr>
2019-10-24 13:19:34 +0200

    merge master

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+var data
+
 function ville(V){
-        fetch('https://www.prevision-meteo.ch/services/json/'+V)
+    fetch('https://www.prevision-meteo.ch/services/json/'+V)
         .then(function(response) {
-          return response.json();
+            return response.json();
         })
-        .then(function(json) {
-        document.body.innerHTML = json.city_info.name;
-          console.log("ville", json)
+        .then(function(json) { 
+            /*document.getElementById('main_div').innerHTML = json.city_info.name + '<br><br>' +
+                                                            json.fcst_day_0.day_long + '<br>' +
+                                                            json.current_condition.tmp + '°' + '<br>' +
+                                                            json.current_condition.condition;*/
+            dom_modif_winfo('main_div', json)
+            // debug
+            data = json
+            console.log("ville", json)
+            console.log('info_city', data)
         });
 }
+
+
 function ip(P){
         var C = document.getElementById("text").value;
         console.log("ip", C) 
@@ -19,3 +30,9 @@ function ip(P){
         });          
 }
     
+
+
+function dom_modif_winfo(elem, json){
+    var doc = document.getElementById(elem)
+    doc.innerHTML = json.city_info.name + '<br><br>'
+}
