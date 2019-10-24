9c41f73ddd5cbc993417f6cc3d0b2c1db739a5cf
theo-oriol <theo.toriol.lol@gmail.com>
2019-10-24 14:55:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,11 @@ function ville(V){
 function ip(P){
         var C = document.getElementById("text").value;
         console.log("ip", C) 
-		
         fetch('https://extreme-ip-lookup.com/json/'+C).then(function(response) { return response.json() }).then(function(json) {  
-            ville(json.city) 
+            ville(json.city)  
+            
 
-		var a = json.country	
-		
-		if(a != "France" ) {
-			alert("Saisissez une adresse IP francaise");
-			document.getElementById('text').value = '';
-			
-		}   
-		
-		}) ;  	      
-		
+
+        });          
 }
+    
