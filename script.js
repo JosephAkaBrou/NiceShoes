83db429fd1a6391d4f67cb7bc0917d21603a1115
pcm <lamouche444@hotmail.fr>
2019-10-24 15:49:48 +0200

    Affichage données météo avec Sylvain

--- script.js
+++ script.js
@@ -29,4 +29,23 @@ function ip(P){
 		}) ;  	      
 		
 }
+
+function dom_modif_winfo(json){
+    var main_div = document.getElementById('main_div')
+    main_div.innerHTML = json.city_info.name+'<br><br><div id="result"></div>'
+
+    for (var i = 0; i <= 3; i++) {
+        var div = document.createElement('div')
+        var br = document.createElement('br')
+        div.setAttribute('class', 'col-md-3')
+        div.appendChild(document.createTextNode(json['fcst_day_'+i].day_long))
+        div.appendChild(document.createElement('br'))
+        div.appendChild(document.createTextNode(json['fcst_day_'+i].tmin+'° - '+json['fcst_day_'+i].tmax+'°'))
+        div.appendChild(document.createElement('br'))
+        div.appendChild(document.createTextNode(json['fcst_day_'+i].condition))
+        var subscribe = document.getElementById('result')
+        var parent = subscribe.parentNode
+        parent.insertBefore(div,subscribe)
+    }
+}
         
