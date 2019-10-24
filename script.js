fd441bd565ad12c9f643b682e5e12f1b53c1baac
pcm <lamouche444@hotmail.fr>
2019-10-24 15:59:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 function ville(V){
         fetch('https://www.prevision-meteo.ch/services/json/'+V)
         .then(function(response) {
-          return response.json();
+          return response.json();          
         })
         .then(function(json) {
-          
-          console.log("ville", json)
+            dom_modif_winfo(json)
+            console.log("ville", json)
         });
 }
 function ip(P){
         var C = document.getElementById("text").value;
         console.log("ip", C) 
-		
+        
         fetch('https://extreme-ip-lookup.com/json/'+C).then(function(response) { return response.json() }).then(function(json) {  
             ville(json.city) 
-
-		var a = json.country	
-		var b = json.city
-		
-		if(a != "France" || document.getElementById('text').value == '') {
-			alert("Saisissez une adresse IP francaise");
-			document.getElementById('text').value = '';			
-		}   
-			
-		}) ;  	      
-		
+        var a = json.country    
+        
+        if(a != "France" || document.getElementById('text').value == '') {
+            alert("Saisissez une adresse IP francaise");
+            document.getElementById('text').value = '';
+            
+        }   
+        
+        }) ;          
 }
 
 function dom_modif_winfo(json){
@@ -43,6 +41,6 @@ function dom_modif_winfo(json){
         var subscribe = document.getElementById('result')
         var parent = subscribe.parentNode
         parent.insertBefore(div,subscribe)
-    }
+    }	
 }
         
