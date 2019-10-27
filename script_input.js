8fd88da6c13e629d7efaa766a52001d9acac4a13
theo-oriol <theo.toriol.lol@gmail.com>
2019-10-27 16:15:18 +0100

    Update script_input.js

--- script_input.js
+++ script_input.js
@@ -1,5 +1,31 @@
 function input(){
 document.getElementsByName("villeTest")[0].innerHTML = "";
 const aff = document.getElementById('main_div');
-aff.innerHTML = "<input class='form-control transparent-input' border = 'none'  background= 'tranparent' type='text' name='name' placeholder='Field title...'  required>";
+aff.innerHTML = "<input id = 'input_check' class='form-control transparent-input' onkeyup='return forceLower(this)'  border = 'none' onkeypress='controle()' background= 'tranparent' type='text' name='name' placeholder='Field title...'  required>";
+}
+
+
+function forceLower(strInput) 
+{
+strInput.value=strInput.value.toLowerCase();
+}
+
+
+function controle(){
+    var P = document.getElementById('input_check').value;
+    console.log(P)
+    if (event.keyCode === 13) {
+        fetch('https://www.prevision-meteo.ch/services/json/'+P).then(function(response) { return response.json() }).then(function(json){
+            var a = json.city_info.country;
+            console.log(a);
+            if(a != "France") {
+                alert("Saisissez le nom d'une ville Française");
+            }
+            else{
+                ville(json.city_info.name) ;
+            }
+        
+        });
+    }
+    
 }
